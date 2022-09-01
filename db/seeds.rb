1a3b3f0211efb968d4dc8014883056b101709524
Anais Augu <augu.anais@gmail.com>
2022-09-01 11:08:39 +0200

    Comment Post.destroy_all in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 puts "Cleaning database..."
-Post.destroy_all
+# Post.destroy_all
 # User.destroy_all
 Information.destroy_all
 
